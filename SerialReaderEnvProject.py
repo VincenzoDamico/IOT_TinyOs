934a49d27c2bddc4883e5dde420b660e7f889f5b
AinelI <63194949+AinelI@users.noreply.github.com>
2025-06-06 15:18:52 +0200

    Added MQTT

--- SerialReaderEnvProject.py
+++ SerialReaderEnvProject.py
@@ -1,7 +1,9 @@
 import serial
 import re
 
-SERIAL_PORT = 'COM4'
+from Publisher.mqttPublisher import MQTTpublisher, addr_broker, port, topic
+
+SERIAL_PORT = 'COM3'
 BAUD_RATE = 115200
 
 sensor_data_dict = {}
@@ -20,17 +22,19 @@ def parse_sensor_line(line):
         try:
             node_id = int(match.group(1))
             temp_raw = int(match.group(2))
-            humidity = int(match.group(3))
+            humidity_raw = int(match.group(3))  
             luminosity = int(match.group(4))
             timestamp = int(match.group(5))
 
             temperature_float = float(temp_raw) / 10.0
-
+            humidity = ((0.0405 + (-2.8E-6 * humidity_raw)) * humidity_raw - 4)
+            hum_round = round(humidity, 1)
+            
             key = (timestamp, node_id)
 
             parsed_data = {
                 "temperature": temperature_float,
-                "humidity": humidity,
+                "humidity": hum_round,
                 "luminosity": luminosity
             }
             return key, parsed_data
@@ -40,6 +44,9 @@ def parse_sensor_line(line):
     return None, None
 
 def leggi_e_processa_dati_seriale():
+
+    publisherMQTT = MQTTpublisher(addr_broker,port,topic)
+
     try:
         with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
             print(f"Connesso a {SERIAL_PORT} a {BAUD_RATE} bps.")
@@ -56,6 +63,17 @@ def leggi_e_processa_dati_seriale():
 
                     if key and parsed_data:
                         sensor_data_dict[key] = parsed_data
+
+                        timestamp, node_id = key #per estrarre i due valori
+
+                        payloadPyt = {
+                            "idSensor": node_id,
+                            "timestamp": timestamp,
+                            **parsed_data
+                        }
+
+                        publisherMQTT.sendMessage(payloadPyt)
+
                         print(f"Aggiunto al dizionario: Chiave={key}, Dati={parsed_data}")
                     print("-" * 50)
 
